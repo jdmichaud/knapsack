a49f44e16c647d1dcb0486029e3de041ccc753a7
jd <jean.daniel.michaud@gmail.com>
2018-03-02 22:34:28 +0100

    Add scoring

--- videos.py
+++ videos.py
@@ -1,12 +1,35 @@
 import sys
-
-def solve():
-  pass
+from functools import reduce
 
 class Endpoint:
-  def __init__(self, dclatency):
-    self.dclatency = 0
-    self.conn = []
+  def __init__(self, id, dclatency, clatencies):
+    self.id = id
+    self.dclatency = dclatency
+    self.clatencies = clatencies
+
+# Score a particular endpoint
+# If videos are seen from this endpoint and the videos are in the cache
+# the score is time saved by streaming from the caches rather than the datacenter
+def score(endpoint, requests, caches):
+  score = 0
+  # FIlter on the requests from this endpoint
+  for (video, eid, nbr) in [(vid, eid, nbr) for (vid, eid, nbr) in requests if eid == endpoint.id]:
+    # Filter on the cache which stores the requested video
+    caches_with_video = [cid for (cid, videos) in caches if video in videos]
+    # Get the latencies of those caches and sort them
+    video_latencies = sorted([latency for (cid, latency) in endpoint.clatencies if cid in caches_with_video])
+    if (len(video_latencies) != 0):
+      # If the video is stored in a cache attached to this point, compute the score
+      score += (endpoint.dclatency - video_latencies[0]) * nbr
+  return score
+
+def score_endpoints(endpoints, requests, caches):
+  return reduce(lambda x, y: x + y, [score(endpoint, requests, caches) for endpoint in endpoints])
+
+def solve(endpoints, requests, C, Csize):
+  caches = [(i, []) for i in range(C)]
+  return caches
+
 
 if __name__ == '__main__':
   if (len(sys.argv) != 2):
@@ -15,17 +38,20 @@ if __name__ == '__main__':
   with open(sys.argv[1], 'r') as ifile:
     V, E, R, C, Csize = [int(x) for x in ifile.readline().split(' ')]
     vsizes = [int(x) for x in ifile.readline().split(' ')]
-    caches = {}
-    endpoints = {}
+    clatencies = {}
+    endpoints = []
     for i in range(E):
-      endpoints[i] = []
       dclatency, ncacheconn = [int(x) for x in ifile.readline().split(' ')]
-      endpoints[i] = dclatency
       for j in range(ncacheconn):
         cid, clatency = [int(x) for x in ifile.readline().split(' ')]
-        if cid not in caches: caches[cid] = []
-        caches[cid].append((i, clatency))
+        if cid not in clatencies: clatencies[cid] = []
+        clatencies[cid] = clatency
+      endpoints.append(Endpoint(i, dclatency, clatencies))
     requests = []
     for i in range(R):
       vid, eid, nbr = [int(x) for x in ifile.readline().split(' ')]
       requests.append((vid, eid, nbr))
+
+    caches = solve(endpoints, requests, C, Csize)
+    score_endpoints(endpoints, requests, caches)
+  
